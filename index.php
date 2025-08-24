8742f5a7a0b34228727dfe49385f2f101fb25773
Alex Balak <alex.balak@outlook.com>
2025-08-24 22:43:35 +0200

    update

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <main class="container mt-4">
+        <form>
+            <div class="mb-3">
+                <label for="product" class="form-label">Nom du produit</label>
+                <input type="text" name="product" class="form-control" id="product">
+            </div>
+            <div class="mb-3">
+                <label for="producer" class="form-label">Fabricant</label>
+                <input type="text" name="producer" class="form-control" id="producer">
+            </div>
+            <div class="mb-3">
+                <label for="species" class="form-label">Espèce</label>
+                <input type="text" name="species" class="form-control" id="species">
+            </div>
+            <div class="mb-3">
+                <label for="packaging" class="form-label">Conditionnement</label>
+                <input type="text" name="packaging" class="form-control" id="packaging">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </main>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</html>
